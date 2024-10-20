2000becf92eb075abe54fe252b575de77d7e6e36
mpinis4 <charalamposxantho@gmail.comm>
2024-10-20 20:46:56 +0300

    Second(Delete-Users-&-Related-Schemas)

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 //Route::get('/products', [ProductController::class, 'index']);
 //Route::post('/products', [ProductController::class, 'store']);
 //Route::get('/products/{product}', [ProductController::class, 'show']);
